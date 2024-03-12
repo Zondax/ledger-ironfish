88db1b21d18447637b259e182cba2c19eff19ec1
ftheirs <fernando@zondax.ch>
2024-03-12 09:47:40 -0300

    add Jubjub

--- app/rust/include/rslib.h
+++ app/rust/include/rslib.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include <stdint.h>
+#include "parser_common.h"
+#include "keys_def.h"
 
-void get_sr25519_sk(uint8_t *sk_ed25519_expanded);
-
-void sign_sr25519_phase1(const uint8_t *sk_ed25519_expanded, const uint8_t *pk, const uint8_t *context_ptr, uint32_t context_len, const uint8_t *msg_ptr, uint32_t msg_len, uint8_t *sig_ptr);
-void sign_sr25519_phase2(const uint8_t *sk_ed25519_expanded, const uint8_t *pk, const uint8_t *context_ptr, uint32_t context_len, const uint8_t *msg_ptr, uint32_t msg_len, uint8_t *sig_ptr);
+/* Interface functions with jubjub crate */
+parser_error_t from_bytes_wide(const uint8_t input[64], uint8_t output[32]);
+parser_error_t scalar_multiplication(const uint8_t input[32], constant_key_t key, uint8_t output[32]);
