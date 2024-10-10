81ae70eb78e80469f30ab6f1fa9bbde20910019b
Carlos Medeiros <carlos.medeiros@zondax.ch>
2024-10-10 17:48:01 +0100

    move decrypt to rust implementation

--- app/rust/include/rslib.h
+++ app/rust/include/rslib.h
@@ -15,8 +15,10 @@ parser_error_t scalar_multiplication(const uint8_t input[32], constant_key_t key
 parser_error_t randomizeKey(const uint8_t key[KEY_LENGTH], const uint8_t randomness[KEY_LENGTH], uint8_t output[KEY_LENGTH]);
 parser_error_t compute_sbar(const uint8_t s[KEY_LENGTH], const uint8_t r[KEY_LENGTH], const uint8_t rsk[KEY_LENGTH],
                             uint8_t sbar[32]);
-parser_error_t shared_secret(uint8_t secret_key[32], uint8_t other_public_key[32], const uint8_t reference_public_key[32],
-                             uint8_t output[32]);
+parser_error_t decrypt_note(const uint8_t *note, uint8_t secret_key[32], uint8_t other_public_key[32],
+                            const uint8_t reference_public_key[32], uint8_t output[32]);
+parser_error_t decrypt_note_encryption_keys(const uint8_t *ovk, const uint8_t *note, uint8_t output[ENCRYPTED_SHARED_KEY_SIZE]);
+
 #ifdef __cplusplus
 }
 #endif
